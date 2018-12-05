ce26b05696aef54ddb4b40db73d092804ed210a4
akaSteroid <alexander-meier83@hotmail.de>
2018-12-05 18:18:24 +0100

    gfhf
    gh

---
+++ Assets/HansWoerstController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HansWoerstController : MonoBehaviour {
+
+    int HansPenisLänge =  3;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Init Woerst");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (true)
+        {
+            Debug.Log("Test" +
+                "");
+        }
+    }
+}
